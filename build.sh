f04283abc2c8c80f0c037ed1e47892df4d48733c
Jonas Frey <dev@jonasfrey.de>
2021-10-20 16:13:05 +0200

    Implemented run.sh Implemented the required detection of the path where the script itself lies

--- build.sh
+++ build.sh
@@ -2,8 +2,21 @@
 
 # Renaming this file from build.sh to build causes a conflict with the build folder from gradle
 
+# Get the full path to the directory of this file
+# Source: https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself/12197518#12197518
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+while([ -h "${SCRIPT_PATH}" ]); do
+    cd "`dirname "${SCRIPT_PATH}"`"
+    SCRIPT_PATH="$(readlink "`basename "${SCRIPT_PATH}"`")";
+done
+cd "`dirname "${SCRIPT_PATH}"`" > /dev/null
+SCRIPT_PATH="`pwd`";
+popd  > /dev/null
+
 # Change into the directory of this file
-cd $(dirname $0)
+cd "${SCRIPT_PATH}"
 
 # Run the gradle build command using the gradle wrapper
+./gradlew clean
 ./gradlew build
--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# Change into the directory of this file
-cd $(dirname $0)
+# Get the full path to the directory of this file
+# Source: https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself/12197518#12197518
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+while([ -h "${SCRIPT_PATH}" ]); do
+    cd "`dirname "${SCRIPT_PATH}"`"
+    SCRIPT_PATH="$(readlink "`basename "${SCRIPT_PATH}"`")";
+done
+cd "`dirname "${SCRIPT_PATH}"`" > /dev/null
+SCRIPT_PATH="`pwd`";
+popd  > /dev/null
 
-# Run the compiler
-echo "Not yet implemented."
-# TODO
+# Change into the directory of this file
+cd "${SCRIPT_PATH}"
+
+# Run the compiler with the given arguments
+java -jar ./build/libs/DerCompiler-*.jar $*
