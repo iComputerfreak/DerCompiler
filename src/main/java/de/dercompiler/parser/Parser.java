785f2a8d060daab902193145eac0a06d488b6a25
Tobias Knorr <Tobias@knorr.lan>
2021-10-26 20:47:00 +0200

    implemented PrecedenceParser algorithm

--- src/main/java/de/dercompiler/parser/Parser.java
+++ src/main/java/de/dercompiler/parser/Parser.java
@@ -1,5 +1,19 @@
 package de.dercompiler.parser;
 
+import de.dercompiler.lexer.Lexer;
+
 public class Parser {
+
+    Lexer lexer;
+    PrecedenceParser precedenceParser;
+
+    public Parser(Lexer lexer) {
+        this.lexer = lexer;
+        precedenceParser = new PrecedenceParser(lexer, this);
+    }
+
+    public void parseUnaryExp() {
+
+    }
 
 }
--- src/main/java/de/dercompiler/parser/PrecedenceParser.java
+++ src/main/java/de/dercompiler/parser/PrecedenceParser.java
@@ -3,16 +3,45 @@ package de.dercompiler.parser;
 import de.dercompiler.lexer.token.IToken;
 import de.dercompiler.lexer.Lexer;
 
+import java.util.Objects;
+
 public class PrecedenceParser {
 
     Lexer lexer;
+    Parser parser;
 
-    public PrecedenceParser(Lexer lexer) {
+    public PrecedenceParser(Lexer lexer, Parser parser) {
         this.lexer = lexer;
     }
 
-    private int presidencOfOperation(IToken token) {
+    private int precedenceOfOperation(IToken token) {
 
         return 0;
+    }
+
+    public void parseExpression() {
+        parseExpression(0);
+    }
+
+    private void parseExpression(int minPrec) {
+        //result
+        parser.parseUnaryExp();
+
+        IToken token = expectOperatorToken();
+        int prec;
+        //TODO really smaller? check after tokens get precedence
+        while (!Objects.isNull(token) && (prec = precedenceOfOperation(token)) < minPrec) {
+            //rhs
+            parseExpression(prec + 1);
+            //result = new Ast(operator, result, rhs);
+            token = expectOperatorToken();
+        }
+        //result
+        return;
+    }
+
+    private IToken expectOperatorToken() {
+        //return null, when 
+        return null;
     }
 }
