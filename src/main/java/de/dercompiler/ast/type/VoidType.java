26d5ec95fdd53d7174b1f040cca367294f8bfbcb
Jonas Frey <dev@jonasfrey.de>
2021-11-02 11:51:18 +0100

    Implemented syntaxEqual functions for types Added SyntaxEquatable interface to separate the syntaxEqual function from the ASTNode

--- src/main/java/de/dercompiler/ast/type/VoidType.java
+++ src/main/java/de/dercompiler/ast/type/VoidType.java
@@ -3,11 +3,11 @@ package de.dercompiler.ast.type;
 import de.dercompiler.ast.ASTNode;
 
 public final class VoidType extends BasicType {
+    
     public VoidType() {}
 
     @Override
     public boolean syntaxEqual(ASTNode other) {
-        // TODO implement
-        return true;
+        return super.syntaxEqual(other) && (other instanceof VoidType);
     }
 }
