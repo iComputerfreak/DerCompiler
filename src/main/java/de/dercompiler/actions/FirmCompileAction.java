5c83bc1efaed0f2b2fab4d6c000f55592905ef20
Tobias Knorr <tkh12@web.de>
2021-12-07 00:29:54 +0100

    implemented compile-firm, we now output assambler

--- src/main/java/de/dercompiler/actions/FirmCompileAction.java
+++ src/main/java/de/dercompiler/actions/FirmCompileAction.java
@@ -1,13 +1,21 @@
 package de.dercompiler.actions;
 
 import de.dercompiler.ast.Program;
+import de.dercompiler.general.GeneralWarningIds;
+import de.dercompiler.generation.CodeGenerationErrorIds;
 import de.dercompiler.io.CommandLineBuilder;
+import de.dercompiler.io.OutputMessageHandler;
 import de.dercompiler.io.Source;
+import de.dercompiler.io.message.MessageOrigin;
 import de.dercompiler.lexer.Lexer;
 import de.dercompiler.parser.Parser;
 import de.dercompiler.pass.PassManager;
 import de.dercompiler.pass.PassManagerBuilder;
 import de.dercompiler.util.ErrorStatus;
+import firm.Backend;
+import firm.Util;
+
+import java.io.IOException;
 
 public class FirmCompileAction extends Action {
     private static final String compilerName = "DerCompiler";
@@ -36,6 +44,16 @@ public class FirmCompileAction extends Action {
         PassManagerBuilder.buildTransformationPipeline(manager);
         manager.run(program);
         ErrorStatus.exitProgramIfError();
+
+        //lower members
+        Util.lowerSels();
+        //maybe add later
+        //Backend.lowerForTarget();
+        try {
+            Backend.createAssembler("a.out", source.filename());
+        } catch (IOException e) {
+            new OutputMessageHandler(MessageOrigin.CODE_GENERATION).printErrorAndExit(CodeGenerationErrorIds.CANT_OUTPUT_FILE, "Can,t write output-file", e);
+        }
     }
 
     public void help() {
