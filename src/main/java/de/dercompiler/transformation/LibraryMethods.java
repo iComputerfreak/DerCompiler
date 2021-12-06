030defade0a474cba1488f681deaf9d20527d37e
Robin Maisch <robinmaisch95@gmail.com>
2021-12-06 23:56:34 +0100

    Rename C library method entity

--- src/main/java/de/dercompiler/transformation/LibraryMethods.java
+++ src/main/java/de/dercompiler/transformation/LibraryMethods.java
@@ -14,7 +14,7 @@ public class LibraryMethods {
     public static final Entity print_byte;
     public static final Entity read_byte;
     public static final Entity flush_out;
-    public static final Entity alloc;
+    public static final Entity allocate;
 
     private static final HashMap<String, Entity> map;
 
@@ -24,10 +24,10 @@ public class LibraryMethods {
         read_byte = new Entity(null, "read_byte", new MethodType(new Type[]{}, new Type[]{FirmTypes.intFirmType}));
         flush_out = new Entity(null, "flush_out", new MethodType(new Type[]{}, new Type[]{}, false));
 
-        alloc = new Entity(null, "alloc", new MethodType(new Type[]{new PointerType(FirmTypes.voidFirmType)}, new Type[]{FirmTypes.intFirmType}));
+        allocate = new Entity(null, "allocate", new MethodType(new Type[]{new PointerType(FirmTypes.voidFirmType)}, new Type[]{FirmTypes.intFirmType}));
 
         map = new HashMap<>(5);
-        List.of(print_int, print_byte, read_byte, flush_out, alloc).stream().forEach(entity -> map.put(entity.getName(), entity));
+        List.of(print_int, print_byte, read_byte, flush_out, allocate).stream().forEach(entity -> map.put(entity.getName(), entity));
     }
 
     public static final Entity forName(String name) {
