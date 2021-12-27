8a532a6a99a367daa4dc22ffe590d87354309e00
Tobias Knorr <tkh12@web.de>
2021-12-27 18:47:14 +0100

    test void type as AnyType

--- src/main/java/de/dercompiler/transformation/FirmTypeFactory.java
+++ src/main/java/de/dercompiler/transformation/FirmTypeFactory.java
@@ -56,7 +56,7 @@ public class FirmTypeFactory {
             // For booleans, we use the byte type (signed or unsigned should not matter)
             return new PrimitiveType(Mode.getBu());
         } else if (type instanceof VoidType) {
-            return null;
+            return new PrimitiveType(Mode.getANY());
         } else if (type instanceof NullType t) {
             return getOrCreateFirmVariableType(t.getExpectedType());
         } else {
