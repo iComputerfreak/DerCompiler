bb3877726a22ef0c77820b840d90d09dbc4c8c1a
Jonas Frey <dev@jonasfrey.de>
2022-01-19 17:46:39 +0100

    Added javadocs

--- src/main/java/de/dercompiler/intermediate/selection/SubstitutionRule.java
+++ src/main/java/de/dercompiler/intermediate/selection/SubstitutionRule.java
@@ -1,6 +1,8 @@
 package de.dercompiler.intermediate.selection;
 
 import de.dercompiler.intermediate.operation.Operation;
+import de.dercompiler.io.OutputMessageHandler;
+import de.dercompiler.io.message.MessageOrigin;
 import firm.Graph;
 import firm.nodes.Cmp;
 import firm.nodes.Node;
@@ -25,13 +27,25 @@ public abstract class SubstitutionRule {
     public SubstitutionRule(Node rootNode) {
         this.rootNode = rootNode;
     }
-    
-    // TODO: Use and add docs
+
+    /**
+     * Sets the NodeAnnotations for the root node and the other required nodes of this rule
+     * @param node The NodeAnnotation of the rootNode
+     * @param annotationSupplier A function that transforms any of the requiredNodes into NodeAnnotations
+     */
     public void setAnnotations(NodeAnnotation node, Function<Node, NodeAnnotation> annotationSupplier) {
+        if (this.node != null || this.annotationSupplier != null) {
+            new OutputMessageHandler(MessageOrigin.CODE_GENERATION)
+                    .internalError("SubstitutionRule.setAnnotations called before clearing existing values.");
+            throw new RuntimeException();
+        }
         this.node = node;
         this.annotationSupplier = annotationSupplier;
     }
-    
+
+    /**
+     * Clears existing NodeAnnotations
+     */
     public void clearAnnotations() {
         this.node = null;
         this.annotationSupplier = null;
@@ -67,8 +81,12 @@ public abstract class SubstitutionRule {
      */
     // Overwritten in subclass
     public abstract List<Node> getRequiredNodes(Graph realGraph);
-    
-    // TODO: Doc
+
+    /**
+     * Checks whether the given input node matches this rule
+     * @param inputNode The input node
+     * @return Whether the rootNode of this rule matches the given input node, including their predecessors
+     */
     public boolean matches(Node inputNode) {
         return matches(getRootNode(), inputNode);
     }
