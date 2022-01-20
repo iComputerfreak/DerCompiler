7c6836f0e6c92e3a97527071de1aab174e994feb
Tobias Knorr <tkh12@web.de>
2022-01-20 20:05:26 +0100

    found bug in libFirm?!? this may be the fix

--- src/main/java/de/dercompiler/ast/expression/ArrayAccess.java
+++ src/main/java/de/dercompiler/ast/expression/ArrayAccess.java
@@ -45,8 +45,9 @@ public final class ArrayAccess extends PostfixExpression {
     @Override
     public ReferenceNode createNode(TransformationState state) {
         ReferenceNode base_ptr = getEncapsulated().createNode(state).prepareAccessArray(state);
-        base_ptr.accessArray(state, state.construction.newConst(0, Mode.getIs()));
+        state.graph.keepAlive(base_ptr.accessArray(state, state.construction.newConst(0, Mode.getIs()));
 
+        base_ptr = base_ptr.prepareAccessArray(state);
         int type_size_const = base_ptr.getType().getFirmType().getSize();
         Node type_size = state.construction.newConst(type_size_const, Mode.getIu());
         ReferenceNode elements = index.createNode(state);
