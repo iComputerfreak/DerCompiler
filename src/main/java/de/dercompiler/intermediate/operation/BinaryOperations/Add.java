0463480577fbe140f40f2365b96bd7b41dd859fd
Thomas <uyfgx@student.kit.edu>
2022-01-22 19:36:06 +0100

    Typfehler bei BinaryOperations/Add und AddRule beispielhaft gelöst

--- src/main/java/de/dercompiler/intermediate/operation/BinaryOperations/Add.java
+++ src/main/java/de/dercompiler/intermediate/operation/BinaryOperations/Add.java
@@ -2,6 +2,7 @@ package de.dercompiler.intermediate.operation.BinaryOperations;
 
 import de.dercompiler.intermediate.operand.Address;
 import de.dercompiler.intermediate.operand.ConstantValue;
+import de.dercompiler.intermediate.operand.Operand;
 import de.dercompiler.intermediate.operand.Register;
 import de.dercompiler.intermediate.operation.BinaryOperation;
 import de.dercompiler.intermediate.operation.OperationType;
@@ -26,5 +27,16 @@ public class Add extends BinaryOperation {
 
     public Add(Address target, ConstantValue source){
         super(OperationType.ADD, target, source);
+    }
+
+    public Add(Operand target, Operand source){
+        super(OperationType.ADD, target, source);
+        if (!(target instanceof Register && source instanceof Register ||
+                target instanceof Register && source instanceof Address ||
+                target instanceof Address && source instanceof Register ||
+                target instanceof Register && source instanceof ConstantValue ||
+                target instanceof Address && source instanceof ConstantValue)){
+            throw new RuntimeException("Operanden von Add sind nicht korrekt");
+        }
     }
 }
--- src/main/java/de/dercompiler/intermediate/selection/rules/AddRule.java
+++ src/main/java/de/dercompiler/intermediate/selection/rules/AddRule.java
@@ -1,7 +1,9 @@
 package de.dercompiler.intermediate.selection.rules;
 
+import de.dercompiler.intermediate.operand.Register;
 import de.dercompiler.intermediate.operation.BinaryOperation;
 import de.dercompiler.intermediate.operation.Operation;
+import de.dercompiler.intermediate.operation.OperationType;
 import de.dercompiler.intermediate.selection.NodeAnnotation;
 import de.dercompiler.intermediate.selection.SubstitutionRule;
 import firm.Graph;
@@ -31,7 +33,7 @@ public class AddRule extends SubstitutionRule<Add> {
 
     @Override
     public List<Operation> substitute() {
-        Operation add = new BinaryOperation(BinaryOperationType.ADD, getLeft().getTarget(), getRight().getTarget());
+        Operation add = new de.dercompiler.intermediate.operation.BinaryOperations.Add(getLeft().getTarget(),getRight().getTarget());
         add.setMode(getRootNode().getMode());
         return List.of(add);
     }
