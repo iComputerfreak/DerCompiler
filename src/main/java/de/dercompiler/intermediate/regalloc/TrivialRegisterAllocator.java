bb66ac540e271012648fef64123e31ded459a157
Thomas <uyfgx@student.kit.edu>
2022-01-26 00:19:48 +0100

    TrivialRegisterAllocator arbeitet auf Ret statements

--- src/main/java/de/dercompiler/intermediate/regalloc/TrivialRegisterAllocator.java
+++ src/main/java/de/dercompiler/intermediate/regalloc/TrivialRegisterAllocator.java
@@ -6,6 +6,8 @@ import de.dercompiler.intermediate.operand.Operand;
 import de.dercompiler.intermediate.operand.ParameterRegister;
 import de.dercompiler.intermediate.operand.VirtualRegister;
 import de.dercompiler.intermediate.operation.BinaryOperation;
+import de.dercompiler.intermediate.operation.NaryOperation;
+import de.dercompiler.intermediate.operation.NaryOperations.Ret;
 import de.dercompiler.intermediate.operation.Operation;
 import de.dercompiler.io.OutputMessageHandler;
 import de.dercompiler.io.message.MessageOrigin;
@@ -26,26 +28,41 @@ public class TrivialRegisterAllocator extends RegisterAllocator{
 
                 //Operand 1 wird geladen
                 if (operands[0] instanceof VirtualRegister vr){
-                    //outputMessageHandler.printInfo();
-                    System.out.println("MOV %r10," + manager.getVar((int) vr.getId()).getIdentifier());
+                    outputMessageHandler.printInfo("MOV %r10," + manager.getVar((int) vr.getId()).getIdentifier());
+
                 } else if (operands[0] instanceof ParameterRegister pr){
-                    System.out.println("MOV %r10," + manager.getArgument((int) pr.getId()).getIdentifier());
+                    outputMessageHandler.printInfo("MOV %r10," + manager.getArgument((int) pr.getId()).getIdentifier());
 
                 }
                 //Operand 2 wird geladen
                 if (operands[1] instanceof VirtualRegister vr){
 
-                    System.out.println("MOV %r11," + manager.getVar((int) vr.getId()).getIdentifier());
+                    outputMessageHandler.printInfo("MOV %r11," + manager.getVar((int) vr.getId()).getIdentifier());
                 } else if (operands[1] instanceof ParameterRegister pr){
-                    System.out.println("MOV %r11," + manager.getArgument((int) pr.getId()).getIdentifier());
+                    outputMessageHandler.printInfo("MOV %r11," + manager.getArgument((int) pr.getId()).getIdentifier());
 
                 }
 
-                System.out.println(op.getOperationType().getSyntax() + " %r10, r11");
+                outputMessageHandler.printInfo(op.getOperationType().getSyntax() + " %r10, r11");
 
+                outputMessageHandler.printInfo("MOV " + manager.getVar((int) ((VirtualRegister) ((BinaryOperation) op).getDefinition()).getId()).getIdentifier() + ", %r10");
 
+            } else if (op instanceof Ret ret){
 
-                System.out.println("MOV " + manager.getVar((int) ((VirtualRegister) ((BinaryOperation) op).getDefinition()).getId()).getIdentifier() + ", %r10");
+                if (ret.getArgs().length != 0){
+                    VirtualRegister returnValue = (VirtualRegister) ret.getArgs()[0];
+
+                    Operand operand = ret.getArgs()[0];
+                    if (operand instanceof VirtualRegister vr){
+
+                        manager.getArgument((int) vr.getId()).getIdentifier();
+                        outputMessageHandler.printInfo("MOV " +  manager.getReturnValue().getIdentifier() + "," + manager.getVar((int) vr.getId()).getIdentifier());
+                    } else if (operand instanceof ParameterRegister pr){
+                        outputMessageHandler.printInfo("MOV " +  manager.getReturnValue().getIdentifier() + "," + manager.getArgument((int) pr.getId()).getIdentifier());
+                    }
+                }
+                outputMessageHandler.printInfo("RET");
+
             }
         }
     }
