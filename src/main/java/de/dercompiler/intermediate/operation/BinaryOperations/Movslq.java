e51fee7804f6803ec933f1253f11201ce523dcf3
Thomas <uyfgx@student.kit.edu>
2022-01-30 21:05:39 +0100

    Movslq suffix entfernt

--- src/main/java/de/dercompiler/intermediate/operation/BinaryOperations/Movslq.java
+++ src/main/java/de/dercompiler/intermediate/operation/BinaryOperations/Movslq.java
@@ -3,6 +3,7 @@ package de.dercompiler.intermediate.operation.BinaryOperations;
 import de.dercompiler.intermediate.operand.Operand;
 import de.dercompiler.intermediate.operation.BinaryOperation;
 import de.dercompiler.intermediate.operation.OperationType;
+import de.dercompiler.intermediate.selection.Datatype;
 
 public class Movslq extends BinArithOperation{
     public Movslq(Operand target, Operand source, boolean isMemoryOperation) {
@@ -12,5 +13,10 @@ public class Movslq extends BinArithOperation{
     @Override
     public BinaryOperation allocate(Operand target, Operand source) {
         return new Movslq(target, source, true);
+    }
+
+    @Override
+    public String getAtntSyntax(Datatype datatype){
+        return operationType.getAtntSyntax(datatype) +  " " + source.getIdentifier(Datatype.WORD) + "," + target.getIdentifier(Datatype.DWORD);
     }
 }
