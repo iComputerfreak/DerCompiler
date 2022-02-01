8c8d54db289bfc118bcd2820da7de1235d6bac33
Robin Maisch <robinmaisch95@gmail.com>
2022-02-01 02:26:33 +0100

    Add Cqto operation

---
+++ src/main/java/de/dercompiler/intermediate/operation/ConstantOperations/Cqto.java
@@ -0,0 +1,20 @@
+package de.dercompiler.intermediate.operation.ConstantOperations;
+
+import de.dercompiler.intermediate.operand.Operand;
+import de.dercompiler.intermediate.operand.X86Register;
+import de.dercompiler.intermediate.operation.ConstantOperation;
+import de.dercompiler.intermediate.operation.Operation;
+import de.dercompiler.intermediate.operation.OperationType;
+import de.dercompiler.intermediate.selection.Datatype;
+import de.dercompiler.intermediate.selection.Signedness;
+
+public class Cqto extends ConstantOperation {
+    /*
+        Operand is implicit. CQTO works on RAX only
+     */
+
+    public Cqto() {
+        super(OperationType.CQTO, true);
+        setMode(Datatype.QWORD, Signedness.UNSIGNED);
+    }
+}
