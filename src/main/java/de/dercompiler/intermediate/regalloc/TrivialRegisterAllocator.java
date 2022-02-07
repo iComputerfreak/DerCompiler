949e8d9b631da15bce35e1e89da9114552d38cbc
Robin Maisch <robinmaisch95@gmail.com>
2022-02-07 21:38:44 +0100

    Fix stack alignment for MacOS 64-bit support

--- src/main/java/de/dercompiler/intermediate/regalloc/TrivialRegisterAllocator.java
+++ src/main/java/de/dercompiler/intermediate/regalloc/TrivialRegisterAllocator.java
@@ -120,7 +120,11 @@ public class TrivialRegisterAllocator extends RegisterAllocator {
         // resetting bp is handled in handleRet
 
         Lea setupStack = new Lea(RSP, manager.getStackEnd());
-        setupStack.setComment("set up stack frame -- %d entries".formatted(manager.getStackSize()));
+
+        // Make stack size an odd number for 64-bit support on MacOS
+        int stackSize = manager.getStackSize();
+        stackSize = stackSize % 2 == 0 ? stackSize + 1 : stackSize;
+        setupStack.setComment("set up stack frame -- %d entries".formatted(stackSize));
         setupOps.add(setupStack);
 
         ops.addAll(0, setupOps);
