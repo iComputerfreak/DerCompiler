9507c1d1b94f0dd827e549fba8f9740841892772
Tobias Knorr <tkh12@web.de>
2022-02-07 22:20:10 +0100

    fixed other conversions

--- src/main/java/de/dercompiler/intermediate/operation/UnaryOperations/Cwtl.java
+++ src/main/java/de/dercompiler/intermediate/operation/UnaryOperations/Cwtl.java
@@ -1,8 +1,11 @@
 package de.dercompiler.intermediate.operation.UnaryOperations;
 
+import de.dercompiler.intermediate.generation.IntelTranslator;
 import de.dercompiler.intermediate.operand.Operand;
 import de.dercompiler.intermediate.operation.OperationType;
 import de.dercompiler.intermediate.operation.UnaryOperation;
+
+import java.util.Objects;
 
 public final class Cwtl extends UnaryArithmeticOperation {
 
@@ -17,5 +20,10 @@ public final class Cwtl extends UnaryArithmeticOperation {
         cwtl.setComment(getComment());
         cwtl.setMode(getMode());
         return cwtl;
+    }
+
+    @Override
+    public String getIntelSyntax() {
+        return "cwde" + (Objects.isNull(operand) ? "" :  " " + operand.acceptTranslator(IntelTranslator.getInstance(), getDatatype()));
     }
 }
